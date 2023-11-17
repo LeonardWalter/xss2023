ffa2523f7903757978a37676fba5cfdf9b98ba7b
Leonard Walter <50204487+LeonardWalter@users.noreply.github.com>
2023-11-18 01:20:20 +0200

    Create y.js

---
+++ y.js
@@ -0,0 +1,20 @@
+const url = 'http://10.110.15.159/forum.php';
+
+const payload = {
+  post_author: 1,
+  post_msg: 'nugget',
+  post_submit: 'Post the message',
+  post_topic_id: 1,
+  token: '41f23d0e891c507a71a1fc0bb51b65b5'
+};
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: new URLSearchParams(payload).toString()
+})
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
